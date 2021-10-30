28073475b1c0144605edd5f704a3df020a4aebf0
qiubit <golpaw1@gmail.com>
2021-10-30 01:28:02 +0100

    Drag and drop video

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,5 +1,44 @@
-const FileUpload = () => (
-  <h1>File Upload Placeholder</h1>
-);
+import { useState } from 'react';
+
+const FileUpload = () => {
+  let [promptText, setPromptText] = useState<string>("Drag a file...")
+  let [videoFile, setVideoFile] = useState<string | null>(null);
+
+  return (
+    <div>
+      <h1
+        onDragEnter={(e) => {
+          e.preventDefault()
+          e.dataTransfer.dropEffect = "none"
+          setPromptText("Drop a file")
+        }}
+        onDragOver={(e) => {
+          e.preventDefault()
+        }}
+        onDragLeave={() => setPromptText("Drag a file...")}
+        onDragEnd={(e) => {
+          e.preventDefault()
+        }}
+        onDrop={(e) => {
+          e.preventDefault()
+          setPromptText("Dropped")
+
+          let file = e.dataTransfer.files[0]
+          if (file != null) {
+            let src = URL.createObjectURL(file);
+            setVideoFile(src)
+          }
+        }}
+      >
+        {promptText}
+      </h1>
+      {videoFile != null ? (
+        <video controls={true} width={640} height={360}>
+          <source src={videoFile} />
+        </video>
+      ): null}
+    </div>
+  )
+}
 
 export default FileUpload;

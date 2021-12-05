d625cd678302de638f241a4c65cb6cc34eeb5a2d
Pawel Golinski <golpaw1@gmail.com>
2021-12-05 16:12:13 +0000

    protect hello API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default withApiAuthRequired(function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const session = getSession(req, res);
   res.status(200).json({ name: 'John Doe' })
-}
+});
